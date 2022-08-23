32a59fe26f60b167189635b456084412b9578531
Oleksandr Shostak <olv.shostak@gmail.com>
2022-08-23 18:46:39 +0200

    add debounce

--- throttle-debounce/throttle-debounce.js
+++ throttle-debounce/throttle-debounce.js
@@ -6,3 +6,36 @@ const handlerScroll = () => {
 
 // divRef.addEventListener("scroll", _.throttle(handlerScroll, 2000));
 // divRef.addEventListener("scroll", _.debounce(handlerScroll, 2000));
+
+function throttle(callback, delay) {
+  let id;
+
+  return function () {
+    // console.log(id);
+    if (id) return;
+
+    id = setTimeout(() => {
+      callback();
+      id = 0;
+    }, delay);
+  };
+}
+
+function debounce(callback, delay) {
+  let id;
+
+  return function () {
+    // console.log(id);
+    if (id) {
+      clearTimeout(id);
+    }
+
+    id = setTimeout(() => {
+      callback();
+      id = 0;
+    }, delay);
+  };
+}
+
+divRef.addEventListener("scroll", throttle(handlerScroll, 2000));
+// divRef.addEventListener("scroll", debounce(handlerScroll, 2000));
