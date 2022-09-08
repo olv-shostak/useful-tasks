e4a67e7666341498969f9fb75998d7fcf2d65e74
Oleksandr Shostak <olv.shostak@gmail.com>
2022-09-08 11:45:16 +0300

    add lazysizes

---
+++ lazysizes/lazysizes.js
@@ -0,0 +1,26 @@
+if ("loading" in HTMLImageElement.prototype) {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+  lazyImages.forEach((image) => {
+    image.src = image.dataset.src;
+  });
+} else {
+  const script = document.createElement("script");
+  script.src =
+    "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+  script.integrity =
+    "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+  script.crossorigin = "anonymous";
+  script.referrerpolicy = "no-referrer";
+  document.body.appendChild("script");
+}
+
+const lazyImages = document.querySelectorAll("img[data-src]");
+
+lazyImages.forEach((image) => {
+  image.addEventListener("load", onImageLoaded, { once: true });
+});
+
+function onImageLoaded(e) {
+  e.target.classList.add("appear");
+}
