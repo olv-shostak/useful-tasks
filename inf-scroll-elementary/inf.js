b14e1dfa13664e5527a8087eb4cc6f4ec41eae8e
Oleksandr Shostak <olv.shostak@gmail.com>
2022-09-09 14:24:57 +0300

    add infinity scroll elementary

---
+++ inf-scroll-elementary/inf.js
@@ -0,0 +1,46 @@
+const COUNTRY_SEGMENT_COUNT = 10;
+let startIndex = 0;
+
+const listRef = document.querySelector(".country-list");
+
+function isScrollToBottom() {
+  return window.innerHeight + window.scrollY >= document.body.offsetHeight;
+}
+
+function createCountryElement(country) {
+  const countryElement = document.createElement("h2");
+  countryElement.classList.add("country");
+  countryElement.textContent = country;
+  return countryElement;
+}
+
+function renderPartOfCountries() {
+  const partOfCountries = countries
+    .slice(startIndex, startIndex + COUNTRY_SEGMENT_COUNT)
+    .map(createCountryElement);
+
+  listRef.append(...partOfCountries);
+
+  startIndex += COUNTRY_SEGMENT_COUNT;
+
+  // console.log(document.body.offsetHeight);
+  // console.log(window.innerHeight);
+  // console.log(window.scrollY);
+  // console.log(startIndex);
+}
+
+renderPartOfCountries();
+if (isScrollToBottom()) renderPartOfCountries();
+
+function scrollHandler() {
+  if (!isScrollToBottom()) return;
+
+  renderPartOfCountries();
+
+  if (startIndex >= countries.length) {
+    window.removeEventListener("scroll", scrollHandler);
+  }
+}
+
+window.addEventListener("scroll", scrollHandler);
+// window.addEventListener("scroll", _.throttle(scrollHandler, 250));
